430b74305797e5a126192f353f4581861ff15f98
rawar <business@wartala.de>
2020-10-24 16:46:20 +0200

    add html 2 text

--- ix2txt.py
+++ ix2txt.py
@@ -6,9 +6,14 @@ from pdfminer.pdfdocument import PDFDocument
 from pdfminer.pdfparser import PDFParser
 from dehyphen import FlairScorer
 from dehyphen.format import text_to_format, format_to_paragraph
+from bs4 import BeautifulSoup
 import io
 import os
 import re
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 filename = '/Users/rwartala/Google Drive/data/ix-archive/daten/19/01/ix1901.pdf'
 years_dict = {
@@ -46,12 +51,26 @@ years_dict = {
     '19': '2019'
 }
 
+def html_to_page_file(html_filename, txt_filename):
+    """
+    Convert a html file to a text file
+    :param html_filename: Name of the HTML file
+    :param txt_filename: Name of the text file
+    :return: None
+    """
+    fp = open(html_filename, 'rb')
+    html_content = fp.read()
+    soup = BeautifulSoup(html_content, features="html.parser")
+    txt_fname = f"{txt_filename}.txt"
+    logging.info(f"convert {html_filename} => {txt_fname}")
+    with open(txt_fname + ".txt", 'wb') as file:
+        file.write(soup.get_text().encode('utf-8'))
+
 
 def pdf_to_page_file(pdf_filename, txt_filename):
     fp = open(pdf_filename, 'rb')
     rsrcmgr = PDFResourceManager()
     retstr = io.StringIO()
-    print(type(retstr))
     # codec = 'utf-8'
     codec = 'iso-8859-1'
     laparams = LAParams()
@@ -62,7 +81,7 @@ def pdf_to_page_file(pdf_filename, txt_filename):
         interpreter.process_page(page)
         data = retstr.getvalue()
         txt_fname = f"{txt_filename}_{page_no}.txt"
-        print(f"convert {pdf_filename} => {txt_fname}")
+        logging.info(f"convert {pdf_filename} => {txt_fname}")
         with open(txt_fname, 'wb') as file:
             file.write(data.encode('utf-8'))
             # file.write(data.encode('iso-8859-1'))
@@ -82,8 +101,8 @@ def get_files_by_pattern(start_dir, pattern):
     return filenames
 
 
-def flatten_output_filenames(start_dir, filenames, years_dict):
-    output_filenames = {}
+def pdf_flatten_output_filenames(start_dir, filenames, years_dict):
+    pdf_output_filenames = {}
     for f_name in filenames:
         if f_name.endswith('.pdf'):
             d_name = os.path.dirname(f_name)
@@ -91,27 +110,59 @@ def flatten_output_filenames(start_dir, filenames, years_dict):
             path, year = os.path.split(path)
             full_year = years_dict[year]
             output_filename = f"ix_{full_year}-{edition}"
-            output_filenames[f_name] = os.path.join(start_dir, output_filename)
+            pdf_output_filenames[f_name] = os.path.join(start_dir, output_filename)
 
-    return output_filenames
+    return pdf_output_filenames
 
 
-def convert_pdf_to_page_files(filenames_dict):
+def html_flatten_output_filenames(start_dir, filenames, years_dict):
+    html_output_filenames = {}
+    for f_name in filenames:
+        if f_name.endswith('art.htm'):
+            d_name = os.path.dirname(f_name)
+            path, page = os.path.split(d_name)
+            path, edition = os.path.split(path)
+            path, year = os.path.split(path)
+            full_year = years_dict[year]
+            output_filename = f"ix_{full_year}-{edition}-{page}"
+            html_output_filenames[f_name] = os.path.join(start_dir, output_filename)
+
+    return html_output_filenames
+
+
+def convert_input_to_page_files(filenames_dict):
     for source_file, target_file in filenames_dict.items():
-        pdf_to_page_file(source_file, target_file)
+        logger.info(f"convert {source_file}...")
+        if source_file.endswith('.htm'):
+            html_to_page_file(source_file, target_file)
+        elif source_file.endswith('.pdf'):
+            pdf_to_page_file(source_file, target_file)
 
 
-def remove_small_files(file_size_max_in_bytes):
-    if os.path.getsize(file) < file_size_max_in_bytes:
-        os.remove(file)
+def remove_small_files(text_files_directory: str, file_size_max_in_bytes: int) -> int:
+    """
+    Remove smaller files with no content.
+    :param text_files_directory: Input directory with all text files
+    :param file_size_max_in_bytes: Number of file size in bytes to remove
+    :return: number of removed files
+    """
+    dirpath, dirs, files = next(os.walk(text_files_directory))
+    file_idx = 0
+    for file in files:
+        if os.path.getsize(file) < file_size_max_in_bytes:
+            file_idx += 1
+            os.remove(file)
+
+    return file_idx
 
 
 def dehyphen_text_file(text_filename) -> str:
     """
     Return a dehyphed text string from a given text file by name.
-    :param text_filename:
-    :return: text string with dehyphed text
+    :param text_filename: Name of the text file
+    :return: Text string with dehyphed text
     """
+    logger.info(f"read text from '{text_filename}'")
     txt_file = open(text_filename, 'r', encoding='utf-8')
     lines = txt_file.readlines()
     text_lines = []
@@ -120,34 +171,70 @@ def dehyphen_text_file(text_filename) -> str:
         if len(line) > 5:
             # remove more then one space in each line
             line = re.sub("\s\s+", " ", line)
+            #line = re.sub("ˇ%", "%")
             ln = [line.strip("\n")]
             text_lines.append(ln)
 
+    logger.info(f"dehyphen {len(text_lines)} lines of text")
     scorer = FlairScorer(lang="de")
     fixed_hyphens = scorer.dehyphen_paragraph(text_lines)
+    logger.info(f"fixed_hyphens='{fixed_hyphens}'")
+    # glue the parts to one text line
+    dehyphed_text = "".join(["".join(fh) + " " for fh in fixed_hyphens])
+    logger.info(f"dehyphed_text='{dehyphed_text}'")
+    return dehyphed_text
 
-    return format_to_paragraph(fixed_hyphens)
+
+def create_csv_from_dir(csv_filename: str, text_files_directory:str):
+    """
+    Create a single column csv file from a text file directory.
+    :param csv_filename: Name of the output csv file
+    :param text_files_directory: Name of the text files directory
+    :return: None
+    """
+    with open(csv_filename, 'w') as csv_file:
+        dirpath, dirs, files = next(os.walk(text_files_directory))
+        number_files = len(files)
+        file_idx = 1
+        logger.info(f"{number_files} file(s) found in {text_files_directory}")
+        for filename in files:
+            logger.info(f"process {file_idx}/{number_files}...")
+            fname = os.path.join(dirpath, filename)
+            if fname.endswith('.txt'):
+                dehyphed_text = dehyphen_text_file(fname)
+                # write one text each line
+                csv_file.write(dehyphed_text + '\n')
+                file_idx += 1
 
 
 def main():
     # input directory for iX archive data
     input_start_dir = '/Users/rwartala/Google Drive/data/ix-archive/daten'
-    # output directory for iX text files 
-    output_start_dir = '/Users/rwartala/Google Drive/data/ix-archive/texts'
+
+    # output directory for iX text files
+    output_start_dir = '/Users/rwartala/Google Drive/data/ix-archive/new_texts'
+
+    # text data file
+    csv_filename = "ix_archive.csv"
+
     # all iX achive pdf files
-    # pdf_files = get_files_by_pattern(input_start_dir, '.pdf')
-    # all iX archive hhtml files
-    # html_files = get_files_by_pattern(input_start_dir, '.htm')
+    pdf_files = get_files_by_pattern(input_start_dir, '.pdf')
+    pdf2txt_dict = pdf_flatten_output_filenames(output_start_dir, pdf_files, years_dict)
+    convert_input_to_page_files(pdf2txt_dict)
 
-    # build up all output file names based on year and edition
-    # pdf2txt_dict = flatten_output_filenames(output_start_dir, pdf_files, years_dict)
 
-    # convert the pdf editions to page-based text files
-    # convert_pdf_to_page_files(pdf2txt_dict)
+    # all iX archive html files
+    #html_files = get_files_by_pattern(input_start_dir, '.htm')
+    #html2txt_dict = html_flatten_output_filenames(output_start_dir, html_files, years_dict)
+    #convert_html_to_page_files(html2txt_dict)
 
-    dehyphen_text = dehyphen_text_file('/Users/rwartala/Google Drive/data/ix-archive/texts/ix_2019-13_99.txt')
-    print(dehyphen_text)
+    # number_removed_files = remove_small_files(output_start_dir, 1)
+    # logger.info(f"remove {number_removed_files} files from {output_start_dir}")
+    #logger.info(f"read text files from {output_start_dir}")
+    #create_csv_from_dir(csv_filename, output_start_dir)
 
+    #text = dehyphen_text_file("/Users/rwartala/Google Drive/data/ix-archive/texts/ix_1991-10_54.txt")
+    #print(text)
 
 if __name__ == "__main__":
     main()
